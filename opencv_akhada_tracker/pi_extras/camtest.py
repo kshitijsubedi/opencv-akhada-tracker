1364a484698f581a0834f3479c52c901c71ce9dd
horizon <2subedikshitij@gmail.com>
2019-01-27 10:27:37 +0545

    .

--- opencv_akhada_tracker/pi_extras/camtest.py
+++ opencv_akhada_tracker/pi_extras/camtest.py
@@ -1,21 +1,9 @@
-import cv2 as cv
+import time
+import picamera
 
-cv.NamedWindow(“w1”, cv.CV_WINDOW_AUTOSIZE)
-camera_index = 0
-capture = cv.CaptureFromCAM(camera_index)
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    time.sleep(0.5)
+    camera.annonate_text="Hey Smile"
 
-def repeat():
-    global capture #declare as globals since we are assigning to them now
-    global camera_index
-    frame = cv.QueryFrame(capture)
-    cv.ShowImage(“w1″, frame)
-    c = cv.WaitKey(10)
-    if(c==”n”): #in “n” key is pressed while the popup window is in focus
-        camera_index += 1 #try the next camera index
-        capture = cv.CaptureFromCAM(camera_index)
-    if not capture: #if the next camera index didn’t work, reset to 0.
-        camera_index = 0
-        capture = cv.CaptureFromCAM(camera_index)
-
-while True:
-repeat()
+pause()
